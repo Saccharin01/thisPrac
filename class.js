2f5ab8305c4690351bdf02ff4bdc23fe3ce1a1df
Saccharin01 <chousik01@gmail.com>
2024-06-04 11:59:20 +0900

    constructor instance method

--- class.js
+++ class.js
@@ -1,1 +1,13 @@
-console.log(`new item class.js`)
+class ItsMe {
+  constructor() {
+    this.name = "조우식",
+    this.age = 30,
+    this.home = "월평동"
+  }
+  sentence(){
+    return this.name + "은 미남이다.";
+  }
+}
+
+let itsMe = new ItsMe()
+console.log(itsMe)
