a587df39b0cb85826f2a2a71400bff8f8f5b74bc
Saccharin01 <chousik01@gmail.com>
2024-06-04 17:37:10 +0900

    json Fetch method inbound

--- jsonfile.js
+++ jsonfile.js
@@ -1,35 +1,12 @@
 const http = require(`http`)
 const fs = require(`fs`)
 
-fetch(`https://infuser.odcloud.kr/oas/docs?namespace=15039555/v1`)
+ fetch('https://api.odcloud.kr/api/15039555/v1/uddi:51c01502-2852-48f8-8382-b8c47f5240f7?page=1&perPage=10&serviceKey=Me%2FMuO55x%2B9wjLDeYl9RoSbEbVNZuF1eg4GGtNtw0ezd8EzDfprXxSxoRQPRSxrKf45kzYf3jCWnPGyQNh2MIQ%3D%3D')
+.then((res)=>{
+  let data = res.json()
+  return data 
+})
 .then((data)=>{
-  // let parseData = JSON.stringify(data)
-  // console.log(parseData)
-
-  // let container = {};
-
   console.log(data)
-
-
-
-
-  // // fs.writeFile(`parseData.json`, data, 'utf-8',(err)=>{
-  // //   if(err){
-  // //     console.log(err)
-  // //   }else{
-  // //     console.log(`data SET!`)
-  // //   }
-  // })
 })
 
-
-
-
-
-
-
-
-
-let server = http.createServer((req, res)=>{
-  
-})
